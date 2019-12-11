2c90931b1aaa9993d81356ec3f9e9356140f3e87
Byeongdo Choi <cookie023@naver.com>
2019-12-11 10:36:06 +0900

    Update CameraShake.cs

--- CameraShake.cs
+++ CameraShake.cs
@@ -1,23 +1,26 @@
-﻿using UnityEngine;
+/*
+ * 프로그램명 : 카메라 흔들림 구현
+ * 작성자 : 2016039033 최병도 (김종우, 박성진, 김서빈, 문재식)
+ * 작성일 : 2019.12.05
+ * 프로그램 설명 : 카메라의 위치를 랜덤적으로 조절하여 카메라를 흔드는 프로그램
+ */
+using UnityEngine;
 using System.Collections;
 
 public class CameraShake : MonoBehaviour
 {
 
-    public bool debugMode = false;//Test-run/Call ShakeCamera() on start
 
-    public float shakeAmount;//The amount to shake this frame.
-    public float shakeDuration;//The duration this frame.
+    public float shakeAmount;//카메라가 흔들리는 양
 
-    //Readonly values...
+ 
     float shakePercentage;//A percentage (0-1) representing the amount of shake to be applied when setting rotation.
-    float startAmount;//The initial shake amount (to determine percentage), set when ShakeCamera is called.
-    float startDuration;//The initial shake duration, set when ShakeCamera is called.
+    float startAmount;//초기 카메라 흔들림 변수
 
     bool isRunning = false; //Is the coroutine running right now?
 
-    public bool smooth;//Smooth rotation?
-    public float smoothAmount = 5f;//Amount to smooth
+    public bool smooth;//카메라를 부드럽게 해주는 변수
+    public float smoothAmount = 5f;//카메라 부드러운 크기
 
     void Start()
     {
@@ -29,21 +32,15 @@ public class CameraShake : MonoBehaviour
     void ShakeCamera()
     {
 
-        startAmount = shakeAmount;//Set default (start) values
-        startDuration = shakeDuration;//Set default (start) values
-
-        if (!isRunning) StartCoroutine(Shake());//Only call the coroutine if it isn't currently running. Otherwise, just set the variables.
+        startAmount = shakeAmount;//카메라 흔들림 양을 받는
     }
 
     public void ShakeCamera(float amount, float duration)
     {
 
-        shakeAmount += amount;//Add to the current amount.
-        startAmount = shakeAmount;//Reset the start amount, to determine percentage.
-        shakeDuration += duration;//Add to the current time.
-        startDuration = shakeDuration;//Reset the start time.
-
-        if (!isRunning) StartCoroutine(Shake());//Only call the coroutine if it isn't currently running. Otherwise, just set the variables.
+        shakeAmount += amount; //카메라 흔들림의 현재 크기를 유지한다.
+        startAmount = shakeAmount;//카메라 흔들림 크기를 유지한다.
+        if (!isRunning) StartCoroutine(Shake());
     }
 
 
@@ -53,15 +50,10 @@ public class CameraShake : MonoBehaviour
 
         while (true)
         {
-            Vector3 rotationAmount = Random.insideUnitSphere * shakeAmount;//A Vector3 to add to the Local Rotation
-            rotationAmount.z = 0;//Don't change the Z; it looks funny.
+            Vector3 rotationAmount = Random.insideUnitSphere * shakeAmount;//카메라의 위치를 랜덤적으로 움직이는 변수
+            rotationAmount.z = 0;//z값이 변하는 정도
 
-            //shakePercentage = shakeDuration / startDuration;//Used to set the amount of shake (% * startAmount).
-
-            //shakeAmount = startAmount * shakePercentage;//Set the amount of shake (% * startAmount).
-            //shakeDuration = Mathf.Lerp(shakeDuration, 0, Time.deltaTime);//Lerp the time, so it is less and tapers off towards the end.
-
-
+            //부드러움 정도를 체크한다.
             if (smooth)
                 transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmount), Time.deltaTime * smoothAmount);
             else
