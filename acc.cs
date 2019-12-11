cf91623b7083e2ab6c97e7f5c60a87afa07ddaea
Byeongdo Choi <cookie023@naver.com>
2019-12-11 10:42:16 +0900

    Update acc.cs

--- acc.cs
+++ acc.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+/*
+ * 프로그램명 : 자동차 엔진음을 나오기 위한 변수
+ * 작성자 : 2016039033 최병도 (김종우, 박성진, 김서빈, 문재식)
+ * 작성일 : 2019.11.30
+ * 프로그램 설명 : 라디오 음악과 차량의 엔진음을 동시에 재생하는 함수
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
