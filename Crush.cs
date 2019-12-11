60bd7af6603db098188543eec8611e782675a3d3
Byeongdo Choi <cookie023@naver.com>
2019-12-11 10:41:28 +0900

    Update Crush.cs

--- Crush.cs
+++ Crush.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+/*
+ * 프로그램명 : 충돌 처리를 하는 프로그램
+ * 작성자 : 2016039033 최병도 (김종우, 박성진, 김서빈, 문재식)
+ * 작성일 : 2019.12.05
+ * 프로그램 설명 : 자동차와 다른 오브젝트 간에 충돌시 처리하는 프로그램
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,10 +16,12 @@ public class Crush : MonoBehaviour
     public AudioClip radio1;
     void Start()
     {
+        //충돌 효과음을 주기 위한 변수
         RadioPlayer = GetComponent<AudioSource>();
     }
     void OnTriggerEnter(Collider other)
     {
+        //충돌시 충돌 효과음이 나온 후 2초 뒤에 종료 화면이 출현된다
         PlayRadio(RadioPlayer);
         Invoke("second", 2.0f);
         
